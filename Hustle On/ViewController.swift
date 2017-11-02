6d4e0a360e81733711b29d91dc36301b8b595628
neil williams <f_u_b_a_r2003@hotmail.com>
2017-11-02 22:13:41 +0000

    Initial Commit

---
+++ Hustle On/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hustle On
+//
+//  Created by Neil on 02/11/2017.
+//  Copyright © 2017 Neil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
